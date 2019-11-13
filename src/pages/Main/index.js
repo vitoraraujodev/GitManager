b414eacdd5c91abbff5b409a57df4327c9b4e09b
vitoraraujodev <vitoraraujo.dev@gmail.com>
2019-11-13 11:00:26 -0300

    Configurando estilo

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
 
-function Main() {
-  return <h1>Main</h1>;
-}
+import { FaGithubAlt, FaPlus } from 'react-icons/fa';
 
-export default Main;
+import { Container, Form, SubmitButton } from './styles';
+
+export default function Main() {
+  return (
+    <Container>
+      <h1>
+        <FaGithubAlt />
+        Repositórios
+      </h1>
+      <Form onSubmit={() => {}}>
+        <input type="text" placeholder="Adicionar repositório" />
+        <SubmitButton>
+          <FaPlus color="#FFF" size={14} />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
+}
